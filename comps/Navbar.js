13803efb1b8ababf5b6f96084f1d71a4f64b322d
SueLee <slee1008@my.bcit.ca>
2023-02-28 14:22:57 -0800

    create navbar

---
+++ comps/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavContainer = styled.div`
+  position: relative;
+  height: 90px;
+  width: 100vw;
+  background-color: yellow
+  justify-content: center;
+  align-items: center;
+`
+const Comps = styled.div`
+  margin: auto 7.5rem;
+  background-color: transparent;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+// const Logo = styled.img`
+//   height: 50%;
+//   background-color: transparent;
+//   cursor: pointer;
+// `
+
+const Logo = styled.div`
+  height: 50%;
+  background-color: transparent;
+  cursor: pointer;
+`
+
+
+
+export default function Navbar() {
+  return (
+    <NavbarCont>
+      <Comps>
+        <Logo onClick={()=> router.push("/")}>Logo</Logo>
+        <Logo>Home</Logo>
+        <Logo>About</Logo>
+      </Comps>
+    </NavbarCont>
+  )
+}
+
