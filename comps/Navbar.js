5f41e2060ffa8a8290f5aaa0f53e4fea5092b747
SueLee <slee1008@my.bcit.ca>
2023-02-28 14:23:43 -0800

    edit nav bar syntax error

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const NavContainer = styled.div`
+const NavbarCont = styled.div`
   position: relative;
   height: 90px;
   width: 100vw;
